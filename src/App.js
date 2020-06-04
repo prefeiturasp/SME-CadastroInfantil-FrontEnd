ac9790211f2b9499ddac418ac1b4cbad77a7714d
Calvin Rossinhole <calvin.masters@gmail.com>
2020-06-04 10:38:01 -0300

    primeiro commit: setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Aqui será desenvolvido o formulário de cadastro infantil.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
