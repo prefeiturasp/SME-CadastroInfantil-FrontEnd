4a1ec5c124032ac70c1f3b81d7f68b553975edb0
Kelwy Oliveira <kelwyduarte@gmail.com>
2022-12-01 18:28:56 -0300

    Inativação do cadastro infantil

--- src/screens/Formulario/index.js
+++ src/screens/Formulario/index.js
@@ -5,8 +5,7 @@ export const Formulario = () => {
   return (
     <div className="card">
       <div className="card-body">
-        O cadastro encontra-se suspenso e estará disponível a partir de
-        xx/xx/xxxx
+        O cadastro encontra-se suspenso e estará disponível a partir de 02/01/2023
       </div>
     </div>
   );
